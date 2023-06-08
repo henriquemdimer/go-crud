0901e1f6ca450859a32d610dae6b7795215f1d99
henriquemdimer <henrique.mdimer@gmail.com>
2023-06-08 00:25:36 -0300

    feat: added dynamic database connection via environment variables

--- db/connection.go
+++ db/connection.go
@@ -2,12 +2,23 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
+	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 func Open() (db *sql.DB, err error) {
-	str := "host=localhost port=5432 user=root password=admin dbname=main sslmode=disable"
+	godotenv.Load()
+
+	host := os.Getenv("DBHOST")
+	port := os.Getenv("DBPORT")
+	dbname := os.Getenv("DBNAME")
+	user := os.Getenv("DBUSER")
+	password := os.Getenv("DBPASSWORD")
+
+	str := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=disable", host, port, dbname, user, password)
 	db, err = sql.Open("postgres", str)
 
 	if err != nil {
