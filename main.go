da3150ce581858d079dd2c72a995b54621b17ebc
henriquemdimer <henrique.mdimer@gmail.com>
2023-06-08 00:28:27 -0300

    feat: initialized server and chi router

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/henriquemdimer/go-crud/controllers"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	r := chi.NewRouter()
+
+	r.Post("/", controllers.Create)
+	r.Get("/", controllers.ReadAll)
+	r.Get("/{todoID}", controllers.ReadOne)
+
+	http.ListenAndServe(":8080", r)
 }
