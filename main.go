ca7aa750e3d63f211f348365b08d20f7c9696dcc
henriquemdimer <henrique.mdimer@gmail.com>
2023-06-08 00:31:16 -0300

    feat: added route to delete todo

--- main.go
+++ main.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/henriquemdimer/go-crud/controllers"
+	"github.com/henriquemdimer/go-crud/db"
 )
 
 func main() {
+	db, err := db.Open()
+	if err != nil {
+		fmt.Println("Error connecting db:", err)
+		os.Exit(2)
+	}
+	db.Close()
+
 	r := chi.NewRouter()
 
 	r.Post("/", controllers.Create)
 	r.Get("/", controllers.ReadAll)
 	r.Get("/{todoID}", controllers.ReadOne)
+	r.Delete("/{todoID}", controllers.Delete)
 
 	http.ListenAndServe(":8080", r)
 }
