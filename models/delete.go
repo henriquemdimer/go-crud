854b7e6714a7d8db75980f96f9778e297f9e018e
henriquemdimer <henrique.mdimer@gmail.com>
2023-06-08 00:26:48 -0300

    feat: added models

---
+++ models/delete.go
@@ -0,0 +1,17 @@
+package models
+
+import "github.com/henriquemdimer/go-crud/db"
+
+func DeleteOne(id int64) (int64, error) {
+	db, err := db.Open()
+	if err != nil {
+		return 0, nil
+	}
+
+	res, err := db.Exec("DELETE FROM todos WHERE id=$1", id)
+	if err != nil {
+		return 0, nil
+	}
+
+	return res.RowsAffected()
+}
---
+++ models/insert.go
@@ -0,0 +1,16 @@
+package models
+
+import "github.com/henriquemdimer/go-crud/db"
+
+func InsertOne(todo Todo) (id int64, err error) {
+	db, err := db.Open()
+	if err != nil {
+		return
+	}
+
+	sql := "INSERT INTO todos (title, description, done) VALUES ($1, $2, $3) RETURNING id"
+	err = db.QueryRow(sql, todo.Title, todo.Description, todo.Done).Scan(&id)
+
+	db.Close()
+	return
+}
