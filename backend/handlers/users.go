30e4dd7e2f8ed2e82f5367290b3d5f73648a0b13
henriquemdimer <henrique.mdimer@gmail.com>
2023-06-20 01:11:29 -0300

    feat: add route to fetch user info

--- backend/handlers/users.go
+++ backend/handlers/users.go
@@ -2,10 +2,15 @@ package handlers
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/henriquemdimer/go-crud/auth"
 	controllers "github.com/henriquemdimer/go-crud/controllers"
 )
 
 func LoadUserRoutes(router chi.Router) {
 	router.Post("/", controllers.CreateUser)
-	router.Post("/login", controllers.GetUser)
+	router.Post("/login", controllers.Login)
+
+	infos := router.Group(nil)
+	infos.Use(auth.Middleware)
+	infos.Get("/", controllers.GetUser)
 }
