6aa30ff492b508c99f8e6b92cf81a16dd0eeb893
henriquemdimer <henrique.mdimer@gmail.com>
2023-06-20 01:15:48 -0300

    feat: implemented create todo function

--- frontend/src/components/Input/index.tsx
+++ frontend/src/components/Input/index.tsx
@@ -1,12 +1,60 @@
+import { FormEvent, useState } from "react";
 import "./style.css";
+import { createTodo } from "../../shared/api/todo";
 
-export function Input() {
+export interface IProps {
+    reloadData: (...args: any[]) => Promise<void>;
+}
+
+export function Input(props: IProps) {
+    const [text, setText] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    async function addTodo(e: FormEvent) {
+        setLoading(true);
+        e.preventDefault();
+        try {
+            const token = localStorage.getItem("token");
+            if (!token) return;
+
+            await createTodo(text, token);
+            await props.reloadData(false);
+
+            const btn = document.getElementById("addtodo");
+            if (btn) {
+                btn.classList.add("active")
+
+                setTimeout(() => {
+                    setText("");
+                    btn.innerText = "Todo adicionado!"
+
+                    const parent = document.getElementById("items");
+                    if (parent) {
+                        parent.scroll({
+                            top: parent.scrollHeight,
+                            behavior: "smooth"
+                        });
+                    }
+                }, 300);
+
+                setTimeout(() => {
+                    btn.classList.remove("active");
+                    btn.innerText = "+";
+                    setLoading(false);
+                }, 2000);
+            }
+        } catch (err) {
+            console.log(err);
+            setLoading(false);
+        }
+    }
+
     return (
         <>
-            <div id="input">
-                <input placeholder="Fazer algo..." />
-                <button>+</button>
-            </div>
+            <form onSubmit={addTodo} id="input">
+                <input value={text} onChange={(e) => setText(e.target.value)} placeholder="Fazer algo..." />
+                <button disabled={loading} id="addtodo" type="submit">+</button>
+            </form>
         </>
     )
 }
