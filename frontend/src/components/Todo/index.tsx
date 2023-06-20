b32783c8c364f53f6341bf9c3f09c3079addb2a2
henriquemdimer <henrique.mdimer@gmail.com>
2023-06-20 01:16:16 -0300

    feat: implemented delete todo & mark done

--- frontend/src/components/Todo/index.tsx
+++ frontend/src/components/Todo/index.tsx
@@ -1,20 +1,63 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "./style.css"
+import { deleteTodo, setTodoDone } from "../../shared/api/todo";
 
-export function Todo() {
-    const [done, setDone] = useState(false)
+export interface IProps {
+    title: string;
+    id: number;
+    done: boolean;
+    reloadData: (...args: any[]) => Promise<void>;
+}
+
+export function Todo(props: IProps) {
+    const [done, setDone] = useState(props.done)
+    const [active, setActive] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setActive(true);
+        }, 100);
+    }, []);
+
+    async function removeTodo() {
+        const token = localStorage.getItem("token");
+        if (!token) return;
+
+        try {
+            await deleteTodo(props.id, token);
+            setActive(false);
+
+            setTimeout(async () => {
+                await props.reloadData(false);
+            }, 500);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    async function markDone() {
+        const token = localStorage.getItem("token");
+        if (!token) return;
+
+        try {
+            setDone(!done);
+            await setTodoDone(props.id, !done, token);
+        } catch (err) {
+            console.log(err);
+        }
+    }
 
     return (
         <>
-            <li className={`todo ${done ? "line" : ""}`}>
-                <div onClick={() => setDone(!done)}>
+            <li className={`todo ${done ? "line" : ""} ${active ? "active" : ""}`}>
+                <div onClick={() => markDone()}>
                     <label className="checkbox">
-                        <input type="checkbox" checked={done} />
+                        <input type="checkbox" onChange={() => { }} checked={done} />
                         <span className="checkmark" />
                     </label>
-                    <p>Terminar de desenvolver este site</p>
+                    <p>{props.title}</p>
                 </div>
-                <div className='x'></div>
+                <div onClick={() => removeTodo()} className='x'></div>
             </li>
         </>
     )
