aab3d52474896acc38d816d1916a07a952263525
henriquemdimer <henrique.mdimer@gmail.com>
2023-06-20 01:14:20 -0300

    feat: add support for loading state

--- frontend/src/components/Button/index.tsx
+++ frontend/src/components/Button/index.tsx
@@ -5,12 +5,15 @@ export interface IProps {
     label: string;
     onClick?: (...args: any[]) => any;
     type?: React.ButtonHTMLAttributes<HTMLButtonElement>['type'];
+    loading?: boolean;
 }
 
 export function Button(props: IProps) {
     return (
         <>
-            <button type={props.type} className="button" onClick={() => props.onClick?.()}>{props.label}</button>
+            <button type={props.type} className="button" onClick={() => props.onClick?.()}>
+                {props.loading ? <img src="loader.svg" width={15} height={15} /> : props.label}
+            </button>
         </>
     )
 }
