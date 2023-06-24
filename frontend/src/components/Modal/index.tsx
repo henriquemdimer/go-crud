52d5a4a7baec7063c929348cdcd4c6178f84a4b3
henriquemdimer <henrique.mdimer@gmail.com>
2023-06-24 01:05:35 -0300

    feat: add option to block modals (cant close)

--- frontend/src/components/Modal/index.tsx
+++ frontend/src/components/Modal/index.tsx
@@ -4,12 +4,13 @@ export interface IProps {
     active: boolean;
     setActive: (state: boolean) => void;
     children: React.ReactNode;
+    blockClose?: boolean;
 }
 
 export function Modal(props: IProps) {
     return (
         <>
-            <div onClick={() => props.setActive(false)} className={`overlay ${props.active ? "active" : ""}`}></div>
+            <div onClick={() => !props.blockClose && props.setActive(false)} className={`overlay ${props.blockClose ? "" : "pointer"} ${props.active ? "active" : ""}`}></div>
             <div className={`modal ${props.active ? "active" : ""}`}>
                 {props.children}
             </div>
