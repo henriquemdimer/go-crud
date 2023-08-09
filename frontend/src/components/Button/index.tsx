37d5e140c147d01eb723581cd54a268452bcf1ae
henriquemdimer <henrique.mdimer@gmail.com>
2023-08-09 09:51:27 -0300

    feat: add support for onClick events & type attribute on button component

--- frontend/src/components/Button/index.tsx
+++ frontend/src/components/Button/index.tsx
@@ -1,13 +1,16 @@
 import "./style.css";
+import React from 'react';
 
 export interface IProps {
     label: string;
+    onClick?: (...args: any[]) => any;
+    type?: React.ButtonHTMLAttributes<HTMLButtonElement>['type'];
 }
 
 export function Button(props: IProps) {
     return (
         <>
-            <button className="button">{props.label}</button>
+            <button type={props.type} className="button" onClick={() => props.onClick?.()}>{props.label}</button>
         </>
     )
 }
