2577ef7350c93e7b5e47100a6889b26b71d22918
henriquemdimer <henrique.mdimer@gmail.com>
2023-08-09 09:50:13 -0300

    refactor: moved auth elements to a dedicated component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,59 +1,15 @@
 import { useState } from 'react';
 import { Input } from './components/Input';
-import { Modal } from './components/Modal';
 import { Todo } from './components/Todo';
 import './styles/App.css';
-import { Button } from './components/Button';
+import { Auth } from './components/Auth';
 
 function App() {
   const [active, setActive] = useState(false);
-  const [method, setMethod] = useState("login")
 
   return (
     <>
-      <Modal active={active} setActive={setActive}>
-        {method == "login" ? (
-          <div id="login" className="auth slideleft">
-            <h3>Entrar na sua conta</h3>
-            <div className="auth__inputs">
-              <div>
-                <label>Nome de usuario</label>
-                <input />
-              </div>
-              <div>
-                <label>Senha</label>
-                <input />
-              </div>
-              <Button label='Logar' />
-              <div className="auth__change">
-                <p>Não possui uma conta? <span onClick={() => setMethod("register")}>Crie uma</span></p>
-              </div>
-            </div>
-          </div>
-        ) : (
-          <div id="register" className="auth slideright">
-            <h3>Crie uma conta</h3>
-            <div className="auth__inputs">
-              <div>
-                <label>Nome de usuario</label>
-                <input />
-              </div>
-              <div>
-                <label>Senha</label>
-                <input />
-              </div>
-              <div>
-                <label>Repetir senha</label>
-                <input />
-              </div>
-              <Button label='Logar' />
-              <div className="auth__change">
-                <p>Já possui uma conta? <span onClick={() => setMethod("login")}>Entre nela</span></p>
-              </div>
-            </div>
-          </div>
-        )}
-      </Modal>
+      <Auth active={active} setActive={setActive} />
       <div id="container">
         <div id="header">
           <h2>TodoList</h2>
